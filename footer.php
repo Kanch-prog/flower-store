aac3981b108e5526b68d9004b8a99926aefccbf8
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2023-07-23 15:56:10 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>blog</h3>
+            <a href="https://ezinearticles.com/?Flower-Pots-and-Their-Benefits&id=4168612">Flower Pots and Their Benefits</a>
+            <a href="https://thisismygarden.com/2023/05/planting-flowers-in-pots/">Planting Flowers In Pots</a>
+            <a href="https://www.realsimple.com/home-organizing/how-to-care-for-potted-plants">How to Care for Potted Plants</a>
+            <a href="https://www.nationalgeographic.com/science/article/big-bloom">The Big Bloom</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+            <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+            <p> <i class="fas fa-envelope"></i> kanchiatwar@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Colmobo, Sri Lanka </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>tuotrials</span> </div>
+
+</section>
